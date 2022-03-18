63dd716f2a7c0191f3cac07698127a98e40220ca
Iloba <scarlettsney@gmail.com>
2022-03-18 09:51:54 +0100

    tawk-to-chat-system-added

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -117,5 +117,18 @@
         <!--//container-fluid-->
     </div>
     <!--//app-content-->
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/623445d41ffac05b1d7f32d0/1fue31idq';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 </div>
 @endsection
